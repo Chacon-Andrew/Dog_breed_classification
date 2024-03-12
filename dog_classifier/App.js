95d447ef9da3826af2dc137792605f741caa80e0
Andrew Chacon <chaconandrew226@gmail.com>
2024-03-12 00:41:24 -0400

    skeleton of doggy app

--- dog_classifier/App.js
+++ dog_classifier/App.js
@@ -1,10 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { useRef, useState } from 'react';
+import { useEffect } from 'react';
+import { Camera, CameraType } from 'expo-camera'
+import { Image } from 'expo-image';
 
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [image, setImage] = useState(null)
+  const [guess, setGuess] = useState("")
+  const cameraref = useRef(null)
+
+  useEffect(() => {
+    if(!permission) requestPermission();
+    setGuess("Berri!!!")
+    console.log("hit")
+  }, [image])
+
+  const takePicture = async () => {
+    if(cameraref){
+      const data = await cameraref.current.takePictureAsync();
+      setImage(data)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hi Nicole I wuvs u!!!</Text>
+      {
+        permission != null ? 
+        <Camera style={styles.camera} type={type} ref={cameraref} onPress={takePicture}>
+          <View style={{position: 'absolute', bottom: 0, marginBottom: 20}}>
+            <TouchableOpacity onPress={takePicture} style={styles.button}>
+              <Image source={require('./assets/baseline_panorama_fish_eye_black_24dp.png')} style={{width: 100, height: 100}} contentFit='contain'/>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+        : <Text>This Application requires the camera to be used</Text>
+      }
+      <Text>{guess}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +51,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  camera: {
+    height: '80%',
+    width: '100%',
+    alignItems: 'center'
+  },
+  button: {
+    height: '100%',
+    width: '100%',
+  }
 });
